2ebd5564a04ad42bc35d76928352f1dcf316a791
sp <sampeppard@gmail.com>
2016-09-16 12:45:27 -0400

    set up controller and views partials

--- app/app.php
+++ app/app.php
@@ -5,4 +5,18 @@
 
     $app = new Silex\Application();
 
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+       'twig.path' => __DIR__."/../views"
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app["twig"]->render("word_frequency.html.twig");
+    });
+
+    $app->post("/", function() use ($app) {
+        $my_repeatCounter = new RepeatCounter;
+        $word_count = $my_repeatCounter->CountRepeats($_POST[""], $_POST[""]);
+        return $app["twig"]->render("word_count.html.twig", array("result" => $word_count));
+    });
+
 ?>
